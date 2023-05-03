6475d5fa7bc32e691a8db404e1817529c782ad51
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-05-03 18:47:38 +0530

    Add application,category,home and login page

---
+++ app/src/main/java/com/example/login_page/CategoryPage.kt
@@ -0,0 +1,11 @@
+package com.example.login_page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_page)
+    }
+}
---
+++ app/src/main/java/com/example/login_page/MyApplications.kt
@@ -0,0 +1,11 @@
+package com.example.login_page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyApplications : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_applications)
+    }
+}

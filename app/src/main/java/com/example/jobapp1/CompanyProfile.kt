441b1348a5fdb777fc83c410eec4db7770f332fb
hirudaka <hirudakakumarasingha@gmail.com>
2023-05-03 19:11:03 +0530

    added company details pages

---
+++ app/src/main/java/com/example/jobapp1/CompanyProfile.kt
@@ -0,0 +1,15 @@
+package com.example.jobapp1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompanyProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_profile)
+
+        //Intent intent = new Intent(this, NewActivity.class);
+        //startActivity(intent);
+    }
+}

75f63347087a30d302bdca2231b2930887442844
IT21221064 <99318000+IT21221064@users.noreply.github.com>
2023-05-04 23:21:38 +0530

    Designed vacancies

---
+++ app/src/main/java/com/example/login_page/AddVacancyPage.kt
@@ -0,0 +1,11 @@
+package com.example.login_page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddVacancyPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_vacancy_page)
+    }
+}
